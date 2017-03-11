fced74635aecbe29306a95db12d31fdc1d584175
Mark Polyakov <spamhereplease@markasoftware.com>
2017-03-10 20:10:09 -0800

    Ready for netlify

--- src/js/hash.js
+++ src/js/hash.js
@@ -1,14 +1,16 @@
 // this file manages the hash portion of the url, which in this case shows which parts are currently being compared
 
+const m = require('mithril');
+
 module.exports = {
     // c => c does the work of geting rid of empty strings, which occurs when there is no parts (empty string input)
     getList: () => location.hash.slice(3).split(',').filter(c => c),
     add: newName => {
         if(!module.exports.getList().includes(newName)) {
-            location.hash = '#!/' + module.exports.getList().concat(newName).join(',');
+            m.route.set('/' + module.exports.getList().concat(newName).join(','));
         }
     },
-    remove: oldName => location.hash = '#!/' + module.exports.getList().filter(c => c !== oldName).join(','),
+    remove: oldName => m.route.set('/' + module.exports.getList().filter(c => c !== oldName).join(',')),
 }
 
 window.hashMan = module.exports;
