978111b32e884546772018af5186d5b52745324d
Mark Polyakov <spamhereplease@markasoftware.com>
2017-04-03 14:15:53 -0700

    Compile to es5 for googlebot support ;(

--- build/build.bash
+++ build/build.bash
@@ -4,10 +4,15 @@
 echo 'Combining specs...'
 node build/gen-specs.js specs /tmp/specs.js public/generated/sitemap.txt
 [[ $1 == 'production' ]] && cssMini='csso' || cssMini='cat'
-[[ $1 == 'production' ]] && jsMini='babili' || jsMini='cat'
 echo 'Bundling scripts...'
 # TODO: maybe add --noparse /tmp/specs.js but last time i tried it didn't make a noticeable difference (but mithril did)
-browserify -r /tmp/specs.js:spec-data --noparse mithril --debug src/js/entry.js | $jsMini > public/generated/bundle.js
+browserify -r /tmp/specs.js:spec-data --noparse mithril --debug src/js/entry.js > public/generated/bundle.js
+if [[ $1 == 'production' ]]
+then
+    echo 'Compiling and minifying scripts...'
+    babel --presets es2015 --plugins transform-exponentiation-operator < public/generated/bundle.js | uglifyjs --compress --mangle --screw-ie8 > /tmp/bundle.js
+    mv /tmp/bundle.js public/generated/bundle.js
+fi
 echo 'Bundling styles...'
 find src/ -name '*.css' | xargs cat | $cssMini > public/generated/all.css
 echo 'Build complete'
