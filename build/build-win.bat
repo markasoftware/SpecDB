e70f779e7c2bd1bff848e154a94698edaea64ddf
Mark Polyakov <mark@markasoftware.com>
2017-08-04 16:39:05 -0700

    Updated build-win.bat to work from build/ directory and double clicking

--- build/build-win.bat
+++ build/build-win.bat
@@ -3,9 +3,9 @@ echo WARNING: You are using Windows, you scrub! The Windows version of the SpecD
 echo If you encounter errors building a branch which you know should work, try using Linux, WSL, or a VM instead!
 echo.
 echo Combining specs...
-node build\gen-specs.js specs %TEMP%\specs.js public\generated\sitemap.txt
+node gen-specs.js ..\specs %TEMP%\specs.js ..\public\generated\sitemap.txt
 echo Bundling scripts...
-call node_modules\.bin\browserify -r %TEMP%\specs.js:spec-data --noparse mithril --debug src\js\entry.js > public\generated\bundle.js
+call ..\node_modules\.bin\browserify -r %TEMP%\specs.js:spec-data --noparse mithril --debug ..\src\js\entry.js > ..\public\generated\bundle.js
 echo Bundling styles...
-copy /y src\css\*.css public\generated\all.css >nul 2>&1
+copy /y ..\src\css\*.css ..\public\generated\all.css >nul 2>&1
 echo Build complete
