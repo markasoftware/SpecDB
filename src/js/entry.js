0ff9fbceadeef610330710ccffe11a855fbb0b33
Mark Polyakov <mark@markasoftware.com>
2017-09-09 16:02:07 -0700

    part selector extended by default on mobile if no parts are selected

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,7 +1,13 @@
 const m = require('mithril');
+const hash = require('./hash.js');
 
 m.route(document.getElementById('spec-viewer'), '/', {
     '/about': require('./about.js'),
     '/:parts...': require('./spec-viewer.js'),
 });
 m.mount(document.getElementById('part-selector'), require('./part-selector.js'));
+
+// extend part selector if no parts are selected on mobile
+if(hash.getList().length === 0) {
+    document.getElementById('mobile-toggle').checked = true;
+}
