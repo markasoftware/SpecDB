dcc79168ff381fee37edf8e03b56315ba5f4970d
Mark Polyakov <mark@markasoftware.com>
2017-09-10 08:48:42 -0700

    Update build.bash

--- build/build.bash
+++ build/build.bash
@@ -6,7 +6,7 @@ node build/gen-specs.js specs /tmp/specs.js public/generated/sitemap.txt
 [[ $1 == 'production' ]] && cssMini='csso' || cssMini='cat'
 echo 'Bundling scripts...'
 # TODO: maybe add --noparse /tmp/specs.js but last time i tried it didn't make a noticeable difference (but mithril did)
-PATH=$(npm bin):$PATH browserify -r /tmp/specs.js:spec-data --noparse mithril --debug src/js/entry.js > public/generated/bundle.js
+browserify -r /tmp/specs.js:spec-data --noparse mithril --debug src/js/entry.js > public/generated/bundle.js
 if [[ $1 == 'production' ]]
 then
     echo 'Compiling and minifying scripts...'
