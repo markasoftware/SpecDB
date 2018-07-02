bfd0891bf7b34d4109b8c8192561b6ae7393e4e6
Mark Polyakov <mark@markasoftware.com>
2018-07-02 12:27:27 -0700

    Refactor authoritative spec coagulation, move inheritance resolving to later in the build process

--- build/combine-specs.js
+++ build/combine-specs.js
@@ -2,20 +2,42 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const readJSON = path => JSON.parse(fs.readFileSync(path, 'utf8'));
+const merger = (a, b) => {
+	if (_.isArray(a)) {
+		return a.concat(b);
+	}
+}
 
 const [ outputFile, sitemapFile, authoritativeFile, ...otherFiles ] = process.argv.slice(2);
 
-const toOutput = {};
-for (let otherFile of otherFiles) {
-	_.merge(toOutput, readJSON(otherFile));
-}
+let toOutput = {};
+_.mergeWith(toOutput, ...otherFiles.map(readJSON), merger);
 
 // authoritativeFile is separate from otherFiles because it is conceptually different
 // additionally, in between other files and authoritative, there will probably be
 // some specially-managed stuff in the future, like benchmark data which needs to know
 // about what parts exist in order to merge properly.
 
-_.merge(toOutput, readJSON(authoritativeFile));
+_.mergeWith(toOutput, readJSON(authoritativeFile), merger);
+
+// INHERITS
+const resolveInheritance = k => {
+	// prefer dedicated inheritance over categories
+	if (`HIDDEN-${k}` in toOutput) {
+		k = `HIDDEN-${k}`;
+	}
+	if ('inherits' in toOutput[k]) {
+		toOutput[k].data = _.mergeWith({},
+			...toOutput[k].inherits.map(resolveInheritance),
+			toOutput[k].data,
+			merger);
+		delete toOutput[k].inherits;
+	}
+	return toOutput[k].data;
+};
+Object.keys(toOutput).forEach(resolveInheritance);
+toOutput = _.pickBy(toOutput, v => !v.hidden);
+
 fs.writeFileSync(outputFile, `module.exports=${JSON.stringify(toOutput)}`, 'utf8');
 
 const fixedSitemapUrls = [
--- build/gen-specs.js
+++ build/gen-specs.js
@@ -1,9 +1,32 @@
 const fs = require('fs');
+const find = require('find');
+const jsYaml = require('js-yaml');
+const _ = require('lodash');
 
-const megaYaml = require('./megayaml');
+const parseYaml = filePath => {
+	let toReturn;
+	try {
+		toReturn = jsYaml.safeLoad(fs.readFileSync(filePath));
+	} catch (e) {
+		console.error(`FATAL: yaml parsing failed for ${filePath}. Aborting. Error: ${e}`);
+		process.exit(1);
+	}
+	return toReturn;
+}
 
 const [basePath, specOutPath] = process.argv.slice(2);
-
-const specs = megaYaml.parse(basePath);
-
-fs.writeFileSync(specOutPath, JSON.stringify(specs));
+const yamls = find.fileSync(/\.yaml$/, basePath);
+// who's ready for some FUNCTIONAL PROGRAMMING???
+const toOutput = _
+	.chain(yamls)
+	.map(parseYaml)
+	// TODO: instead of spoofing the names, either switch to:
+	// a) separate names for inheritors than actual categories -- probably best option
+	// b) different `data` and `abstractData` properties to allow things to coexist
+	// c) change subtext generation system so we don't need separate data and abstractData
+	// basically, since "hidden" yamls have the same names as non-hidden ones, we need to
+	// give them different names here, then we prefer them in combine-specs.js
+	.keyBy(c => c.hidden ? `HIDDEN-${c.name}` : c.name)
+	.mapValues(v => { delete v.name; return v })
+	.value();
+fs.writeFileSync(specOutPath, JSON.stringify(toOutput));
